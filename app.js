20a301dd5538ebaabc2c59bfd5b119768ddac930
Manoj Neupane <maanojneupane111@gmail.com>
2025-09-26 11:18:01 +0545

    setup in ejs and tailwind css

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ import { fileURLToPath } from "url";
 import path from "path";
 import cookieParser from "cookie-parser";
 
-
 const app = express();
 
 const __filename = fileURLToPath(import.meta.url);
@@ -15,13 +14,10 @@ app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 app.use(cookieParser());
-app.get('/', async(req, res)=>{
-    res.render('index')
-})
+
+app.get('/', async (req, res) => {
+    res.render('index');
+});
 
 
-
-app.listen('3000', ()=>{
-    console.log('Server start in http://localhost:3000');
-    
-})
+export default app;
