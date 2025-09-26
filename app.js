5e88ca8ad2c7257e8057081d543885fa4424c390
Manoj Neupane <maanojneupane111@gmail.com>
2025-09-26 11:23:17 +0545

    setup in ejs and tailwind css

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 import express from "express";
-import { fileURLToPath } from "url";
 import path from "path";
-import cookieParser from "cookie-parser";
-
-const app = express();
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+const app = express();
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-app.use(cookieParser());
-
-app.get('/', async (req, res) => {
-    res.render('index');
+// Serve the index.html file
+app.get("/", (req, res) => {
+    res.render('index', { name: 'Manoj Neupane' });
 });
 
-
-export default app;
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
