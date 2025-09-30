50425ac7211b3940ce923c51b1a273fc6d1eef77
Manoj Neupane <maanojneupane111@gmail.com>
2025-09-30 16:03:00 +0545

    initial push

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-// Serve the index.html file
 app.get("/", (req, res) => {
     res.render('index');
 });
